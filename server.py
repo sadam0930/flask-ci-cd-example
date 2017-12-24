55b210b6e58bc43a32020e0b2aa4ba4ca485ec1c
Steven Adam <sna219@nyu.edu>
2017-12-24 13:56:28 -0500

    update unit tests to test get and post

--- server.py
+++ server.py
@@ -41,6 +41,7 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            flash('Upload succesful')
             return redirect(url_for('uploaded_file',
                                     filename=filename))
     # toDo: return a template
--- test_server.py
+++ test_server.py
@@ -2,25 +2,55 @@
 # python -m unittest discover
 # nosetests -v --rednose --nologcapture
 
+import os
 import unittest
 import server
 import logging
+from flask_api import status    # HTTP Status Codes
+from StringIO import StringIO
+
+UPLOAD_FOLDER = 'static/img'
 
 ######################################################################
 #  T E S T   C A S E S
 ######################################################################
 class TestServer(unittest.TestCase):
-    def setUp(self):
-        server.app.debug = True
-        self.app = server.app.test_client()
+	@classmethod
+	def setUpClass(self):
+		try:
+			os.remove(os.path.join(UPLOAD_FOLDER, 'test.png'))
+		except Exception:
+			log = logging.getLogger('No file to remove.. continuing')
 
-    # toDo: come back and create a meaningful test
-    def test_index(self):
-        resp = self.app.get('/')
-        self.assertTrue (True == True)
+	@classmethod
+	def tearDownClass(self):
+		try:
+			os.remove(os.path.join(UPLOAD_FOLDER, 'test.png'))
+		except Exception:
+			log = logging.getLogger('No file to remove.. continuing')
+
+	def setUp(self):
+		server.app.debug = True
+		self.app = server.app.test_client()
+
+	def test_get_form(self):
+		log = logging.getLogger("Test GET form")
+		resp = self.app.get('/')
+		self.assertTrue('Upload new File' in resp.data)
+		self.assertTrue(resp.status_code == status.HTTP_200_OK)
+
+	def test_post_upload(self):
+		log = logging.getLogger("Test POST upload")
+		# grab test image
+		with open(os.path.join(UPLOAD_FOLDER, 'newapp-icon.png')) as test:
+			imgStringIO = StringIO(test.read())
+
+		data=dict( {'file': (imgStringIO, 'test.png')} )
+		resp = self.app.post('/', data=data, follow_redirects=True, content_type='multipart/form-data')
+		self.assertTrue('Upload succesful', resp.data)
 
 ######################################################################
 #   M A I N
 ######################################################################
 if __name__ == '__main__':
-    unittest.main()
+	unittest.main()
